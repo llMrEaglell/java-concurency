fd7d9f337b4336e3972a14054c0997bed5142d2d
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-12-14 15:43:50 +0200

    Add month on russian

--- NewsParser/src/main/java/newsAgregator/dateParser/DateParser.java
+++ NewsParser/src/main/java/newsAgregator/dateParser/DateParser.java
@@ -4,40 +4,57 @@ import java.time.LocalDate;
 
 public class DateParser {
     public static LocalDate parse(String date) {
-        String[] dates = date.split(" ");
-        int day = Integer.parseInt(dates[1]);
-        int month = getMonth(dates[2]);
-        int year = Integer.parseInt(dates[3]);
-        return LocalDate.of(year, month, day);
+        if (date.equals(" Сегодня")) return LocalDate.now();
+        if (date.equals(" Вчера")) return LocalDate.now().minusDays(1);
+        else {
+            String[] dates = date.split(" ");
+            int day = Integer.parseInt(dates[1]);
+            int month = getMonth(dates[2]);
+            int year = Integer.parseInt(dates[3]);
+            return LocalDate.of(year, month, day);
+        }
     }
 
     private static int getMonth(String month) {
         switch (month) {
             case "січня":
+            case "январь":
                 return 1;
             case "лютого":
+            case "февраль":
                 return 2;
             case "березня":
+            case "март":
                 return 3;
             case "квітня":
+            case "апрель":
                 return 4;
             case "травня":
+            case "май":
                 return 5;
             case "червня":
+            case "июнь":
                 return 6;
             case "липня":
+            case "июль":
                 return 7;
             case "серпня":
+            case "август":
                 return 8;
             case "вересня":
+            case "сентябрь":
                 return 9;
             case "жовтня":
+            case "октябрь":
                 return 10;
             case "листопада":
+            case "ноябрь":
                 return 11;
             case "грудня":
+            case "декабря":
                 return 12;
             default:
+                System.out.println(month);
                 return 0;
         }
     }
---
+++ NewsParser/src/main/java/newsAgregator/parsers/KorrespondentNewsPageParser.java
@@ -0,0 +1,2 @@
+package newsAgregator.parsers;public class KorrespondentNewsPageParser {
+}
