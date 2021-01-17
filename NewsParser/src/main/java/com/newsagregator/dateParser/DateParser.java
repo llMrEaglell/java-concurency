abaa46a54b6cce28c0e00c99ac293aa0c09ebe13
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-01-17 15:27:09 +0200

    Add date parse for strana.ua

--- NewsParser/src/main/java/com/newsagregator/dateParser/DateParser.java
+++ NewsParser/src/main/java/com/newsagregator/dateParser/DateParser.java
@@ -8,13 +8,25 @@ import java.util.Locale;
 public class DateParser {
     public static LocalDate parse(String date) {
         if (date.equals(" Сегодня") || date.equals("сегодня")) return LocalDate.now();
-        if (date.equals(" Вчера")|| date.equals("вчера")) return LocalDate.now().minusDays(1);
+        if (date.equals(" Вчера") || date.equals("вчера")) return LocalDate.now().minusDays(1);
         else {
             DateTimeFormatter formatter = new DateTimeFormatterBuilder()
                     .parseCaseInsensitive()
                     .appendPattern("d MMMM yyyy")
                     .toFormatter(new Locale("ru"));
             return LocalDate.parse(date.substring(1), formatter);
+        }
+    }
+
+    public static LocalDate parseDate(String date) {
+        if (date.equals(" Сегодня") || date.equals("сегодня")) return LocalDate.now();
+        if (date.equals(" Вчера") || date.equals("вчера")) return LocalDate.now().minusDays(1);
+        else {
+            DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                    .parseCaseInsensitive()
+                    .appendPattern("d MMMM yyyy")
+                    .toFormatter(new Locale("ru"));
+            return LocalDate.parse(date, formatter);
         }
     }
 }
