4976ef124cf8fa8fa3d094f77b1e8f5c6f5178a8
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-01-28 17:35:45 +0200

    Remove throws execption. Add try catch IOExecetpion

--- NewsParser/src/main/java/com/newsagregator/NewsSiteProperties.java
+++ NewsParser/src/main/java/com/newsagregator/NewsSiteProperties.java
@@ -6,19 +6,32 @@ import java.util.Properties;
 
 public interface NewsSiteProperties {
     String getBaseURL();
+
     String getPostItemTitle();
+
     String getNewsClass();
+
     String getItemBigPhotoIMG();
+
     String getPostItemText();
+
     String getTimeClass();
+
     String getTagsItemClass();
+
     String getBlockWithNews();
+
     int getConnectionLimit();
 
-    default Properties loadProperties(String fileName) throws IOException {
+    default Properties loadProperties(String fileName) {
         InputStream resource = Main.class.getClassLoader().getResourceAsStream(fileName);
         Properties properties = new Properties();
-        properties.load(resource);
+        try {
+            properties.load(resource);
+        } catch (IOException e) {
+            System.err.println("Can't load properties:" + fileName);
+        }
+
         return properties;
     }
 
